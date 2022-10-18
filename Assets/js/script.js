2f0af73cd4151fce398bb738ca0a440544e5efca
AKJ <kjajila@gmail.com>
2022-10-17 21:48:23 -0400

    add event listner for button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -23,16 +23,32 @@ var searchBtn=document.getElementById("searchBtn");
 //to get city name from user  
 searchBtn.addEventListener("click", function(){
      searchCity=document.getElementById("search").value;
+     cityWeather(searchCity);    
+    
+     // to create buttons and append
+     var buttonTag = document.createElement("button");
+     const textForBtn = document.createTextNode(searchCity);
+     buttonTag.appendChild(textForBtn);
+     document.getElementById("button").appendChild(buttonTag);
+});
+
+
+// event handler for buttons
+button.addEventListener("click", function(event){
+     searchCity=event.target.textContent;
+     cityWeather(searchCity);    
+});
+
+
+// function to get current weather and next 5 day
+function cityWeather(searchCity){
      document.getElementById("city").textContent=searchCity;
-     //to store the search city name to local storage
-     previousCity.push(searchCity);
-     localStorage.setItem("previousCity",previousCity);
      queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&units=imperial&appid=" + ApiKey;
 
      //to get data for a particular city
      fetch(queryURL)
      .then(function (response) {
-     return response.json();
+          return response.json();
      })
      .then(function (data) {
           document.getElementById("city-temp").textContent=data.main.temp+" F";
@@ -41,40 +57,29 @@ searchBtn.addEventListener("click", function(){
           latitude=data.coord.lat;
           longitude=data.coord.lon;
           var icon=data.weather[0].icon;
-          //console.log(icon);
           var imgUrl="https://openweathermap.org/img/wn/"+icon+"@2x.png";
           document.getElementById("icon").setAttribute("src",imgUrl);
 
           //to get data for 5 days for that particular city
           queryUrlFive="https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=imperial&appid=" + ApiKey;
-          //console.log(queryUrlFive);
           fetch(queryUrlFive)
           .then(function (response) {
-          return response.json();
+               return response.json();
           })
           .then(function (dataSet) {
-          var c=1;
-          for (var i = 0; i <=dataSet.cnt; i+=9) {
-               var date=(dataSet.list[i].dt_txt).split(" ");
-               document.getElementById("date"+c).textContent=date[0];
-               var icon=dataSet.list[i].weather[0].icon;
-               //console.log(icon);
-               var imgUrl="https://openweathermap.org/img/wn/"+icon+"@2x.png";
-               document.getElementById("icon"+c).setAttribute("src",imgUrl);
-               //document.getElementById("icon"+c).textContent=dataSet.list[i].weather.description;
-               document.getElementById("temp"+c).textContent=dataSet.list[i].main.temp+" F";
-               document.getElementById("wind"+c).textContent=dataSet.list[i].wind.speed+" MPH";
-               document.getElementById("humidity"+c).textContent=dataSet.list[i].main.humidity+" %";
-               c++;
-          }
-
-          // to create buttons and append
-          var buttonTag = document.createElement("button");
-          const textForBtn = document.createTextNode(searchCity);
-          buttonTag.appendChild(textForBtn);
-          document.getElementById("button").appendChild(buttonTag);
-
+               var c=1;
+               for (var i = 0; i <=dataSet.cnt; i+=9) {
+                    var date=(dataSet.list[i].dt_txt).split(" ");
+                    document.getElementById("date"+c).textContent=date[0];
+                    var icon=dataSet.list[i].weather[0].icon;
+                    var imgUrl="https://openweathermap.org/img/wn/"+icon+"@2x.png";
+                    document.getElementById("icon"+c).setAttribute("src",imgUrl);
+                    //document.getElementById("icon"+c).textContent=dataSet.list[i].weather.description;
+                    document.getElementById("temp"+c).textContent=dataSet.list[i].main.temp+" F";
+                    document.getElementById("wind"+c).textContent=dataSet.list[i].wind.speed+" MPH";
+                    document.getElementById("humidity"+c).textContent=dataSet.list[i].main.humidity+" %";
+                    c++;
+               }
+          });
      });
-     });
-});
-
+}
